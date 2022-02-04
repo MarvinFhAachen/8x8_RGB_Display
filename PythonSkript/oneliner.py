a358ab6fdb622df5eca1eba5cfb81bfa22f04ef8
Marvin <56964320+MarvinFhAachen@users.noreply.github.com>
2022-02-04 18:03:07 +0100

    Python skript + website eingebaut

---
+++ PythonSkript/oneliner.py
@@ -0,0 +1,26 @@
+liste2 =[""]
+
+print("start")
+ 
+with open('Website.html') as file:
+    liste = [line.rstrip() for line in file]
+
+#print(str(len(liste)))
+#print("start bereinigen") 
+liste2.append("\"")
+for x in liste:
+    if   not x.find("//")> (-1) and not x.find("<!--")> (-1):
+        x = x.replace("\"", "\\\"")
+        x = x.replace("\'", "\\\'")
+        liste2.append(x)
+liste2.append("\"")
+
+
+
+#print("start write")
+f=open("website_one.txt","w")
+for y in liste2:
+    f.write(y)
+    #f.write("\n")
+f.close()
+print("done")
