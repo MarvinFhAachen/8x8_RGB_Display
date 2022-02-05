87a9f7058171481d634c5b58517a46dd628e22ff
Marvin <56964320+MarvinFhAachen@users.noreply.github.com>
2022-02-05 20:37:04 +0100

    fix website auf mcu

--- PythonSkript/oneliner.py
+++ PythonSkript/oneliner.py
@@ -7,20 +7,24 @@ with open('Website.html') as file:
 
 #print(str(len(liste)))
 #print("start bereinigen") 
-liste2.append("\"")
+#liste2.append("\"")
 for x in liste:
     if   not x.find("//")> (-1) and not x.find("<!--")> (-1):
         x = x.replace("\"", "\\\"")
         x = x.replace("\'", "\\\'")
         liste2.append(x)
-liste2.append("\"")
+#liste2.append("\"")
 
 
 
 #print("start write")
 f=open("website_one.txt","w")
 for y in liste2:
+    f.write("send +=\"")
     f.write(y)
-    #f.write("\n")
+
+    f.write(" \\n \";")
+    f.write("\n")
+    
 f.close()
 print("done")
